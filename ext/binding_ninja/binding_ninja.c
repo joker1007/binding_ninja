26d1406ba46e675b883e90368669ff5a5cd24c5f
joker1007 <kakyoin.hierophant@gmail.com>
2017-09-24 23:10:09 +0900

    Add :if option to `auto_inject_binding`

--- ext/binding_ninja/binding_ninja.c
+++ ext/binding_ninja/binding_ninja.c
@@ -3,11 +3,38 @@
 VALUE rb_mBindingNinja;
 static VALUE auto_inject_binding_invoke(int argc, VALUE *argv, VALUE self);
 
+static ID options_id;
+
 static VALUE
-auto_inject_binding(VALUE mod, VALUE method_sym)
+auto_inject_binding(int argc, VALUE *argv, VALUE mod)
 {
     ID mid;
-    VALUE mod_name, extensions, ext_mod;
+    VALUE mod_name, extensions, ext_mod, method_sym, options, opt, cond;
+    static ID extensions_id;
+    static ID keyword_ids[1];
+
+    if (!keyword_ids[0]) {
+      keyword_ids[0] = rb_intern("if");
+    }
+
+    if (!extensions_id) {
+      extensions_id = rb_intern("@auto_inject_binding_extensions");
+    }
+
+    if (rb_cvar_defined(mod, options_id)) {
+      options = rb_cvar_get(mod, options_id);
+    } else {
+      options = rb_hash_new();
+      rb_cvar_set(mod, options_id, options);
+    }
+
+    rb_scan_args(argc, argv, "1:", &method_sym, &opt);
+    if (!NIL_P(opt)) {
+      rb_get_kwargs(opt, keyword_ids, 0, 1, &cond);
+      if (cond != Qundef) {
+        rb_hash_aset(options, method_sym, cond);
+      }
+    }
 
     mid = SYM2ID(method_sym);
     int arity = rb_mod_method_arity(mod, mid);
@@ -15,18 +42,17 @@ auto_inject_binding(VALUE mod, VALUE method_sym)
       rb_raise(rb_eArgError, "target method receives 1 or more arguments");
     }
 
-    mod_name = rb_mod_name(mod);
-    extensions = rb_ivar_get(rb_mBindingNinja, rb_intern("@auto_inject_binding_extensions"));
-    ext_mod = rb_hash_aref(extensions, mod_name);
-    if (ext_mod == Qnil) {
+    extensions = rb_ivar_get(rb_mBindingNinja, extensions_id);
+    ext_mod = rb_hash_aref(extensions, mod);
+    if (NIL_P(ext_mod)) {
       ext_mod = rb_module_new();
-      rb_hash_aset(extensions, mod_name, ext_mod);
+      rb_hash_aset(extensions, mod, ext_mod);
     }
+
     if (rb_mod_include_p(mod, ext_mod) == Qfalse) {
       rb_prepend_module(mod, ext_mod);
     }
-
-    rb_define_method_id(ext_mod, SYM2ID(method_sym), auto_inject_binding_invoke, -1);
+    rb_define_method_id(ext_mod, mid, auto_inject_binding_invoke, -1);
 
     return method_sym;
 }
@@ -34,12 +60,31 @@ auto_inject_binding(VALUE mod, VALUE method_sym)
 static VALUE
 auto_inject_binding_invoke(int argc, VALUE *argv, VALUE self)
 {
-  VALUE binding, args_ary;
+  VALUE method_sym, options, binding, args_ary, klass, cond;
 
-  binding = rb_binding_new();
+  method_sym = ID2SYM(rb_frame_this_func());
+  options = rb_cvar_get(CLASS_OF(self), options_id);
+
+  cond = rb_hash_lookup2(options, method_sym, Qtrue);
+
+  if (rb_obj_is_proc(cond)) {
+    if (abs(rb_proc_arity(cond)) > 0) {
+      cond = rb_proc_call(cond, rb_ary_new_from_args(1, self));
+    } else {
+      cond = rb_proc_call(cond, rb_ary_new());
+    }
+  } else if (SYMBOL_P(cond)) {
+    VALUE method_argv[0];
+    cond = rb_method_call(0, method_argv, rb_obj_method(self, cond));
+  }
+
   args_ary = rb_ary_new_from_values(argc, argv);
-  rb_ary_unshift(args_ary, binding);
-
+  if (RTEST(cond)) {
+    binding = rb_binding_new();
+    rb_ary_unshift(args_ary, binding);
+  } else {
+    rb_ary_unshift(args_ary, Qnil);
+  }
   return rb_call_super(argc + 1, RARRAY_CONST_PTR(args_ary));
 }
 
@@ -47,6 +92,7 @@ void
 Init_binding_ninja(void)
 {
   rb_mBindingNinja = rb_define_module("BindingNinja");
+  options_id = rb_intern("@@__auto_inject_binding_options");
   rb_ivar_set(rb_mBindingNinja, rb_intern("@auto_inject_binding_extensions"), rb_hash_new());
-  rb_define_private_method(rb_mBindingNinja, "auto_inject_binding", auto_inject_binding, 1);
+  rb_define_private_method(rb_mBindingNinja, "auto_inject_binding", auto_inject_binding, -1);
 }
--- spec/binding_ninja_spec.rb
+++ spec/binding_ninja_spec.rb
@@ -13,19 +13,77 @@ RSpec.describe BindingNinja do
       b
     end
 
+    def foo3(b, a = 1)
+      b.local_variables if b
+    end
+
+    def foo4(b, a = 1)
+      b.local_variables if b
+    end
+
+    def foo5(b)
+      b.local_variables if b
+    end
+
+    def enable_auto_inject_binding?
+      true
+    end
+
     auto_inject_binding :foo
     auto_inject_binding :foo2
+    auto_inject_binding :foo3, if: ENV["ENABLE_BINDING_NINJA"]
+    auto_inject_binding :foo4, if: ->(obj) { obj.class.to_s != "Foo" }
+    auto_inject_binding :foo5, if: :enable_auto_inject_binding?
+
+    alias :hoge :foo2
+  end
+
+  class Bar < Foo
+    def enable_auto_inject_binding?
+      false
+    end
+  end
+
+  klass = Class.new do
+    extend BindingNinja
+    auto_inject_binding def foo(b)
+      b.local_variables
+    end
   end
 
   it "inject binding" do
-    hoge = 1
-    obj = Foo.new
-    expect(obj.foo(1, 2, 3, 4, abc: 2, def: 10)).to match_array([:hoge, :obj])
-    obj.foo2
-    expect(hoge).to eq(100)
+    aggregate_failures do
+      hoge = 1
+      obj = Foo.new
+      expect(obj.foo(1, 2, 3, 4, abc: 2, def: 10)).to match_array([:hoge, :obj, :klass])
+      obj.hoge
+      expect(hoge).to eq(100)
+      if ENV["ENABLE_BINDING_NINJA"]
+        puts "ENABLE_BINDING_NINJA=#{ENV["ENABLE_BINDING_NINJA"]}"
+        expect(obj.foo3).to match_array([:hoge, :obj, :klass])
+      else
+        expect(obj.foo3).to be_nil
+      end
+      expect(obj.foo4).to be_nil
+      expect(obj.foo5).to match_array([:hoge, :obj, :klass])
+      expect(Bar.new.foo4).to match_array([:hoge, :obj, :klass])
+      expect(Bar.new.foo5).to be_nil
+      expect(klass.new.foo).to match_array([:hoge, :obj, :klass])
+    end
   end
 
   it "create extension module" do
-    expect(BindingNinja.send(:instance_variable_get, "@auto_inject_binding_extensions")["Foo"]).to be_a(Module)
+    expect(BindingNinja.send(:instance_variable_get, "@auto_inject_binding_extensions")[Foo]).to be_a(Module)
+    expect(Foo.send(:class_variable_get, "@@__auto_inject_binding_options")).to match({
+      :foo3 => nil,
+      :foo4 => an_instance_of(Proc),
+      :foo5 => :enable_auto_inject_binding?,
+    })
+    expect(Bar.send(:class_variable_get, "@@__auto_inject_binding_options")).to match({
+      :foo3 => nil,
+      :foo4 => an_instance_of(Proc),
+      :foo5 => :enable_auto_inject_binding?,
+    })
+    expect(klass.send(:class_variable_get, "@@__auto_inject_binding_options")).to match({})
   end
 end
